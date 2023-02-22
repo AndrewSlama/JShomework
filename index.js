25ae5c31713a80e7338657b9ce2eaa1d7a043b43
AndrewSlama <117046824+AndrewSlama@users.noreply.github.com>
2023-02-21 20:26:00 -0600

    idk

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector(".alertbox");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  //All Characters
+  var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+{}<>[]';
+  //Length
+  var passwordLegth = 128;
+  var password = '';
 
+  for (password i = 0; i < passwordLegth; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);    alertbox.innerHTML = "New PassWord Copied: <br> " + password;
+    }
+    document.getElementById("password").value = password;
+  }
   passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
