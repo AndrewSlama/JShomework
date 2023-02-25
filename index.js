af494218337041c0a1415995dad3b0a693ff4512
AndrewSlama <117046824+AndrewSlama@users.noreply.github.com>
2023-02-25 15:58:03 -0600

    Added prompts (confirm)

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
-  var specialChar = ["! ","@","#","$","%","^","&","*","(",")","{","}",">","<","?","~"];
-  var letterChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var specialChar = ["!","@","#","$","%","^","&","*","(",")","{","}",">","<","?","~"];
+  var lowerChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var numbChar = ["1","2","3","4","5","6","7","8","9","0"];
-
+  var length = 0;
+  var isSpecialChar = false;
+  var isLowerChar = false;
+  var isUpperChar = false;
+  var isNumbChar = false;
+  generateBtn.addEventListener("click", writePassword);
 // Write password to the #password input
 function writePassword() {
 getPrompts();
@@ -28,10 +33,15 @@ getPrompts();
   passwordText.value = password;
 }
 function getPrompts(){
-  var length = prompt("Please enter the desired password length. Only numberical values 8-128");
+
+  length = prompt("Please enter the desired password length. Only numberical values 8-128");
+  isSpecialChar = confirm("Please select if you would like special characters");
+  isLowerChar = confirm("Please select if you would like lowercase character");  
+  isUpperChar = confirm("Please select if you would like uppercase character");
+  isNumbChar = confirm("Please select if you would like number characters"); 
 
 }
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
 
 
